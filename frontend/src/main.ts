4617a7a4b9808b0ce726616f06801335441d5b16
Yulong Cai <yumoucyl@gmail.com>
2023-05-27 20:12:59 +1000

    Gradient Background

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,18 +1,18 @@
-import "./assets/main.css"
+import "./assets/main.css";
 
-import { createApp } from "vue"
-import { createPinia } from "pinia"
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from "./App.vue"
-import router from "./router"
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-const meta = document.createElement("meta")
-meta.name = "naive-ui-style"
-document.head.appendChild(meta)
+const meta = document.createElement("meta");
+meta.name = "naive-ui-style";
+document.head.appendChild(meta);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount("#app")
+app.mount("#app");
