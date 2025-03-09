41e33118541e980746fbc903f57b476283ae06f4
Long <62230884+darkmid@users.noreply.github.com>
2025-03-09 18:52:24 +0800

    update proxy setting for url prefixing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,10 +15,10 @@ export default defineConfig({
   server: {
     port: 8080,
     proxy: {
-      "/api": {
-        target: "http://127.0.0.1:3000/api",
+      "/api/v1": {
+        target: "http://127.0.0.1:3000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path,
       },
     },
   },
